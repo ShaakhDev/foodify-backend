a266916d6a4623905c3d8a083fe3be9e02c3edd2
Shakhzod Bobolov <shohbobolov98@gmail.com>
2023-03-11 23:04:04 +0500

    bcrypt

--- index.js
+++ index.js
@@ -19,58 +19,58 @@ app.get("/", (req, res) => {
 	res.send("Express on Vercel");
 });
 
-app.get("/api/geocode", (req, res) => {
-	const { city, mock } = req.query;
-	if (mock === "true") {
-		const locationMock = locations[city.toLowerCase()];
-		return res.status(200).json(locationMock);
-	}
+// app.get("/api/geocode", (req, res) => {
+// 	const { city, mock } = req.query;
+// 	if (mock === "true") {
+// 		const locationMock = locations[city.toLowerCase()];
+// 		return res.status(200).json(locationMock);
+// 	}
 
-	client
-		.geocode({
-			params: {
-				address: city,
-				key: process.env.GOOGLE_CLOUD_API_KEY,
-			},
-			timeout: 1000, // milliseconds
-		})
-		.then(result => {
-			return res.status(200).json(result.data);
-		})
-		.catch(e => {
-			return res.status(400).json(e.response.data.error_message);
-		});
-});
+// 	client
+// 		.geocode({
+// 			params: {
+// 				address: city,
+// 				key: process.env.GOOGLE_CLOUD_API_KEY,
+// 			},
+// 			timeout: 1000, // milliseconds
+// 		})
+// 		.then(result => {
+// 			return res.status(200).json(result.data);
+// 		})
+// 		.catch(e => {
+// 			return res.status(400).json(e.response.data.error_message);
+// 		});
+// });
 
-app.get("/api/placesNearby", (req, res) => {
-	const { location, mock, placeType } = req.query;
+// app.get("/api/placesNearby", (req, res) => {
+// 	const { location, mock, placeType } = req.query;
 
-	if (mock === "true") {
-		const data = mocks[location];
-		if (data) {
-			data.results = data.results.map(addMockImage);
-		}
-		return res.status(200).json(data);
-	}
+// 	if (mock === "true") {
+// 		const data = mocks[location];
+// 		if (data) {
+// 			data.results = data.results.map(addMockImage);
+// 		}
+// 		return res.status(200).json(data);
+// 	}
 
-	client
-		.placesNearby({
-			params: {
-				location,
-				radius: 1500,
-				type: placeType || "restaurant",
-				key: process.env.GOOGLE_CLOUD_API_KEY,
-			},
-			timeout: 1000, // milliseconds
-		})
-		.then(result => {
-			let data = result.data.results.map(addGoogleImage);
-			result.data.results = data;
+// 	client
+// 		.placesNearby({
+// 			params: {
+// 				location,
+// 				radius: 1500,
+// 				type: placeType || "restaurant",
+// 				key: process.env.GOOGLE_CLOUD_API_KEY,
+// 			},
+// 			timeout: 1000, // milliseconds
+// 		})
+// 		.then(result => {
+// 			let data = result.data.results.map(addGoogleImage);
+// 			result.data.results = data;
 
-			return res.status(200).json(result?.data);
-		})
-		.catch(e => res.status(400).json(e.response.data.error_message));
-});
+// 			return res.status(200).json(result?.data);
+// 		})
+// 		.catch(e => res.status(400).json(e.response.data.error_message));
+// });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
